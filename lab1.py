4f065b18383fbc26607042ee06cc6a98a8d871e6
Jack Geiger <107611621+jggeiger@users.noreply.github.com>
2023-09-13 21:56:59 -0400

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,7 +1,13 @@
 import requests
 
+#URL to Google
+URLGoogle = "https://google.com/"
+
+#Get URL
+resp = requests.get(URLGoogle)
+
 #URL to GitHub code
-URL = "https://raw.githubusercontent.com/jggeiger/CMPUT404_lab01/main/lab1.py"
+URLGitHub = "https://raw.githubusercontent.com/jggeiger/CMPUT404_lab01/main/lab1.py"
 
 #Get URL and print text
 resp = requests.get(URL)
