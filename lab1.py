d3ebf9e7634dc242fe1f3d72090ccf5cc87ea3ab
Jack Geiger <jggeiger@cs.ualberta.ca>
2023-09-13 19:39:55 -0600

    added self printing

--- lab1.py
+++ lab1.py
@@ -1,9 +1,8 @@
 import requests
 
-print(requests.__version__)
+#URL to GitHub code
+URL = "https://raw.githubusercontent.com/jggeiger/CMPUT404_lab01/main/lab1.py"
 
-#raw python code
-URL = "https://raw.githubusercontent.com/jggeiger/CMPUT404_lab01/main/lab1.py?token=GHSAT0AAAAAACHR6S7223YGECSQ7SXCD4JYZICMJNA"
-
-#resp = requests.get("http://google.com")
-#print(resp.text)
+#Get URL and print text
+resp = requests.get(URL)
+print(resp.text)
